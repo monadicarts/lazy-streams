00d7f791da6348870834c7afb6415da358f62a81
Justin Frost <justin@patch.tech>
2024-11-14 16:12:31 -0700

    chore(release): 1.0.14

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2018,
     sourceType: "module",
   },
   plugins: ["@typescript-eslint", "prettier"],
